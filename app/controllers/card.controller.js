de66f870c947a33614b3b151ea149204e248e9dc
leerivera <leerivera80@icloud.com>
2021-10-20 18:30:52 -0400

    findone crud

--- app/controllers/card.controller.js
+++ app/controllers/card.controller.js
@@ -57,6 +57,23 @@ exports.findAll = (req, res) => {
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Card.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find card with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id
+      });
+    });
   
 };
 
